2242631986e5e4e07083c1e141c387214feaccf4
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-11-12 13:18:03 -0500

    add basic REPL capabilities

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,28 @@
 module Main where
 
+import System.Environment(getArgs)
+import System.IO(hFlush, stdout)
+import System.Exit(die)
 import Lexer
 import Parser
 import Eval
 
-greeting :: IO ()
-greeting = putStrLn "Welcome to dater, date calculations done easy!"
-
 main :: IO ()
-main = greeting
+main = do
+  args <- getArgs
+  if null args then do
+    putStrLn "Welcome to dater, date calculations done easy!"
+    until_ (readPrompt "dater$>") putStrLn (== ":q")
+  else
+    die "Unrecognized arguments"
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+  where flushStr str = putStr str >> hFlush stdout
+
+until_ :: (Monad m) => m a -> (a -> m()) -> (a -> Bool) -> m ()
+until_ prompt action isQuit = do
+  input <- prompt
+  case isQuit input of
+    True -> return ()
+    _    -> action input >> until_ prompt action isQuit
